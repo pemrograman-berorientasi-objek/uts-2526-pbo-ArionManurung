047f64a163f201f3351982fce1b5288d1f2d506b
ArionManurung <arionmanurung44@gmail.com>
2026-03-27 17:06:44 +0700

    12S24014-Arion Manurung

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -1,15 +1,45 @@
 package academic.driver;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24014 Arion Manurung
  */
+
+import academic.model.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Driver1 {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        ArrayList<Account> listAccount = new ArrayList<>();
 
-    public static void main(String[] _args) {
+        while (true) {
+            String data = inputScanner.nextLine();
 
-        // codes
+            // kondisi berhenti
+            if (data.equals("---")) {
+                break;
+            }
 
+            String[] splitData = data.split("#");
+
+            if (splitData[0].equals("create-account")) {
+                String accName = splitData[1];
+                String accUsername = splitData[2];
+
+                Account newAccount = new Account(accName, accUsername);
+                listAccount.add(newAccount);
+            }
+        }
+
+        for (Account account : listAccount) {
+            System.out.println(
+                account.getUsername() + "|" +
+                account.getName() + "|" +
+                account.getBalance()
+            );
+        }
+
+        inputScanner.close();
     }
-
 }
--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,15 +1,93 @@
 package academic.driver;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S24014 Arion Manurung
  */
+
+import academic.model.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Driver2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    public static void main(String[] _args) {
+        ArrayList<Account> accountList = new ArrayList<>();
+        ArrayList<Transaction> transactionList = new ArrayList<>();
 
-        // codes
+        int trxIdCounter = 0;
 
+        while (true) {
+            String line = input.nextLine();
+
+            if (line.equals("---")) {
+                break;
+            }
+
+            String[] data = line.split("#");
+            String cmd = data[0];
+
+            if (cmd.equals("create-account")) {
+                String accName = data[1];
+                String accUsername = data[2];
+
+                Account account = new Account(accName, accUsername);
+                accountList.add(account);
+            }
+
+            else if (cmd.equals("deposit")) {
+                String user = data[1];
+                double nominal = Double.parseDouble(data[2]);
+                String time = data[3];
+                String note = data[4];
+
+                for (Account account : accountList) {
+                    if (account.getUsername().equals(user)) {
+                        account.deposit(nominal);
+
+                        trxIdCounter++;
+                        Transaction depositTrx = new DepositTransaction(
+                            trxIdCounter, user, nominal, time, note
+                        );
+                        transactionList.add(depositTrx);
+                        break;
+                    }
+                }
+            }
+
+            else if (cmd.equals("withdraw")) {
+                String user = data[1];
+                double nominal = Double.parseDouble(data[2]);
+                String time = data[3];
+                String note = data[4];
+
+                for (Account account : accountList) {
+                    if (account.getUsername().equals(user)) {
+
+                        boolean isSuccess = account.withdraw(nominal);
+
+                        if (isSuccess) {
+                            trxIdCounter++;
+                            Transaction withdrawTrx = new WithdrawTransaction(
+                                trxIdCounter, user, nominal, time, note
+                            );
+                            transactionList.add(withdrawTrx);
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (Account account : accountList) {
+            System.out.println(
+                account.getUsername() + "|" +
+                account.getName() + "|" +
+                account.getBalance()
+            );
+        }
+
+        input.close();
     }
-
 }
